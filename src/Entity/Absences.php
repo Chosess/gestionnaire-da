415173dc3e3a29eeb01a58323ab7a0d5a76db8d0
Chosess <n.noelle552@gmail.com>
2023-09-20 15:42:20 +0200

    entity absence

---
+++ src/Entity/Absences.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AbsencesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AbsencesRepository::class)]
+class Absences
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $debut = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $fin = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $motif = null;
+
+    #[ORM\Column]
+    private ?bool $justif = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $document = null;
+
+    #[ORM\ManyToOne(inversedBy: 'absences')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Eleves $eleves = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): static
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): static
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getMotif(): ?string
+    {
+        return $this->motif;
+    }
+
+    public function setMotif(string $motif): static
+    {
+        $this->motif = $motif;
+
+        return $this;
+    }
+
+    public function isJustif(): ?bool
+    {
+        return $this->justif;
+    }
+
+    public function setJustif(bool $justif): static
+    {
+        $this->justif = $justif;
+
+        return $this;
+    }
+
+    public function getDocument(): ?string
+    {
+        return $this->document;
+    }
+
+    public function setDocument(string $document): static
+    {
+        $this->document = $document;
+
+        return $this;
+    }
+
+    public function getEleves(): ?Eleves
+    {
+        return $this->eleves;
+    }
+
+    public function setEleves(?Eleves $eleves): static
+    {
+        $this->eleves = $eleves;
+
+        return $this;
+    }
+}
--- src/Entity/Eleves.php
+++ src/Entity/Eleves.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ElevesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ElevesRepository::class)]
@@ -40,11 +41,87 @@ class Eleves
     #[ORM\OneToMany(mappedBy: 'eleves', targetEntity: Documents::class)]
     private Collection $documents;
 
+    #[ORM\OneToMany(mappedBy: 'eleves', targetEntity: Absences::class)]
+    private Collection $absences;
+
+    #[ORM\Column]
+    private ?bool $validation_inscription = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_inscription = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $formation = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $niveau_formation = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $annee_formation = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prescripteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $conseiller = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $code_postal = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ville = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $portable = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fixe = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom_urgence = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom_urgence = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telephone_urgence = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lieu_naissance = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_naissance = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nationalite = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $etat_civil = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $enfants = null;
+
+    #[ORM\Column]
+    private ?bool $ordinateur = null;
+
+    #[ORM\Column]
+    private ?bool $sport = null;
+
+    #[ORM\Column]
+    private ?bool $droit_image = null;
+
     public function __construct()
     {
         $this->entretiens = new ArrayCollection();
         $this->transports = new ArrayCollection();
         $this->documents = new ArrayCollection();
+        $this->absences = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -198,6 +275,324 @@ class Eleves
                 $document->setEleves(null);
             }
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Absences>
+     */
+    public function getAbsences(): Collection
+    {
+        return $this->absences;
+    }
+
+    public function addAbsence(Absences $absence): static
+    {
+        if (!$this->absences->contains($absence)) {
+            $this->absences->add($absence);
+            $absence->setEleves($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAbsence(Absences $absence): static
+    {
+        if ($this->absences->removeElement($absence)) {
+            // set the owning side to null (unless already changed)
+            if ($absence->getEleves() === $this) {
+                $absence->setEleves(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function isValidationInscription(): ?bool
+    {
+        return $this->validation_inscription;
+    }
+
+    public function setValidationInscription(bool $validation_inscription): static
+    {
+        $this->validation_inscription = $validation_inscription;
+
+        return $this;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->date_inscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $date_inscription): static
+    {
+        $this->date_inscription = $date_inscription;
+
+        return $this;
+    }
+
+    public function getFormation(): ?string
+    {
+        return $this->formation;
+    }
+
+    public function setFormation(string $formation): static
+    {
+        $this->formation = $formation;
+
+        return $this;
+    }
+
+    public function getNiveauFormation(): ?string
+    {
+        return $this->niveau_formation;
+    }
+
+    public function setNiveauFormation(string $niveau_formation): static
+    {
+        $this->niveau_formation = $niveau_formation;
+
+        return $this;
+    }
+
+    public function getAnneeFormation(): ?int
+    {
+        return $this->annee_formation;
+    }
+
+    public function setAnneeFormation(int $annee_formation): static
+    {
+        $this->annee_formation = $annee_formation;
+
+        return $this;
+    }
+
+    public function getPrescripteur(): ?string
+    {
+        return $this->prescripteur;
+    }
+
+    public function setPrescripteur(string $prescripteur): static
+    {
+        $this->prescripteur = $prescripteur;
+
+        return $this;
+    }
+
+    public function getConseiller(): ?string
+    {
+        return $this->conseiller;
+    }
+
+    public function setConseiller(string $conseiller): static
+    {
+        $this->conseiller = $conseiller;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(string $code_postal): static
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPortable(): ?string
+    {
+        return $this->portable;
+    }
+
+    public function setPortable(string $portable): static
+    {
+        $this->portable = $portable;
+
+        return $this;
+    }
+
+    public function getFixe(): ?string
+    {
+        return $this->fixe;
+    }
+
+    public function setFixe(string $fixe): static
+    {
+        $this->fixe = $fixe;
+
+        return $this;
+    }
+
+    public function getNomUrgence(): ?string
+    {
+        return $this->nom_urgence;
+    }
+
+    public function setNomUrgence(string $nom_urgence): static
+    {
+        $this->nom_urgence = $nom_urgence;
+
+        return $this;
+    }
+
+    public function getPrenomUrgence(): ?string
+    {
+        return $this->prenom_urgence;
+    }
+
+    public function setPrenomUrgence(string $prenom_urgence): static
+    {
+        $this->prenom_urgence = $prenom_urgence;
+
+        return $this;
+    }
+
+    public function getTelephoneUrgence(): ?string
+    {
+        return $this->telephone_urgence;
+    }
+
+    public function setTelephoneUrgence(string $telephone_urgence): static
+    {
+        $this->telephone_urgence = $telephone_urgence;
+
+        return $this;
+    }
+
+    public function getLieuNaissance(): ?string
+    {
+        return $this->lieu_naissance;
+    }
+
+    public function setLieuNaissance(string $lieu_naissance): static
+    {
+        $this->lieu_naissance = $lieu_naissance;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $date_naissance): static
+    {
+        $this->date_naissance = $date_naissance;
+
+        return $this;
+    }
+
+    public function getNationalite(): ?string
+    {
+        return $this->nationalite;
+    }
+
+    public function setNationalite(string $nationalite): static
+    {
+        $this->nationalite = $nationalite;
+
+        return $this;
+    }
+
+    public function getEtatCivil(): ?string
+    {
+        return $this->etat_civil;
+    }
+
+    public function setEtatCivil(string $etat_civil): static
+    {
+        $this->etat_civil = $etat_civil;
+
+        return $this;
+    }
+
+    public function getEnfants(): ?int
+    {
+        return $this->enfants;
+    }
+
+    public function setEnfants(int $enfants): static
+    {
+        $this->enfants = $enfants;
+
+        return $this;
+    }
+
+    public function isOrdinateur(): ?bool
+    {
+        return $this->ordinateur;
+    }
+
+    public function setOrdinateur(bool $ordinateur): static
+    {
+        $this->ordinateur = $ordinateur;
+
+        return $this;
+    }
+
+    public function isSport(): ?bool
+    {
+        return $this->sport;
+    }
+
+    public function setSport(bool $sport): static
+    {
+        $this->sport = $sport;
+
+        return $this;
+    }
+
+    public function isDroitImage(): ?bool
+    {
+        return $this->droit_image;
+    }
+
+    public function setDroitImage(bool $droit_image): static
+    {
+        $this->droit_image = $droit_image;
 
         return $this;
     }
